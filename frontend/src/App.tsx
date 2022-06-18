a96115b19da668a31a40de0cbf82fd8fd1c0fb41
Axel Prieto <prietoaxel@gmail.com>
2022-06-18 11:38:50 +0100

    Daily programming practice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,132 +1,110 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 import {useTable} from 'react-table';
+import './App.css';
 
 function App() {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    [],
-  );
+  const [data, setResult] = useState([]);
+  let [loading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    loading = true;
+    fetch('http://localhost:3005/logs')
+      .then((response) => response.json())
+      .then((data) => {
+        setResult(data.rows);
+        loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+        loading = false;
+      });
+  };
+
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Column 1',
-        accessor: 'col1', // accessor is the "key" in the data
+        Header: 'Date',
+        accessor: 'doc.date', // accessor is the "key" in the data
       },
       {
-        Header: 'Column 2',
-        accessor: 'col2',
+        Header: 'File',
+        accessor: 'doc.file',
       },
     ],
     [],
   );
+
   // @ts-ignore
   const tableInstance = useTable({columns, data});
 
-  // ***
-  const getLogs = async () => {
-    const response = await fetch('http://localhost:3005', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'GET',
-    });
-    if (response.status !== 400) {
-      const content = await response.json();
-      +console.log(content);
-    }
-  };
-  getLogs();
-
-  // return (
-  //   <div className='App'>
-  //     <header className='App-header'>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className='App-link'
-  //         href='https://reactjs.org'
-  //         target='_blank'
-  //         rel='noopener noreferrer'
-  //       >
-  //         LARIANLARIANLARIAN2
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
   const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} =
     tableInstance;
 
   return (
-    // apply the table props
-    <table {...getTableProps()}>
-      <thead>
-        {
-          // Loop over the header rows
-          headerGroups.map((headerGroup) => (
-            // Apply the header row props
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {
-                // Loop over the headers in each row
-                headerGroup.headers.map((column) => (
-                  // Apply the header cell props
-                  <th {...column.getHeaderProps()}>
-                    {
-                      // Render the header
-                      column.render('Header')
-                    }
-                  </th>
-                ))
-              }
-            </tr>
-          ))
-        }
-      </thead>
-      {/* Apply the table body props */}
-      <tbody {...getTableBodyProps()}>
-        {
-          // Loop over the table rows
-          rows.map((row) => {
-            // Prepare the row for display
-            prepareRow(row);
-            return (
-              // Apply the row props
-              <tr {...row.getRowProps()}>
-                {
-                  // Loop over the rows cells
-                  row.cells.map((cell) => {
-                    // Apply the cell props
-                    return (
-                      <td {...cell.getCellProps()}>
+    <>
+      {loading ? (
+        <div>Data Loading...</div>
+      ) : (
+        // apply the table props
+        <table {...getTableProps()}>
+          <thead>
+            {
+              // Loop over the header rows
+              headerGroups.map((headerGroup) => (
+                // Apply the header row props
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {
+                    // Loop over the headers in each row
+                    headerGroup.headers.map((column) => (
+                      // Apply the header cell props
+                      <th {...column.getHeaderProps()}>
                         {
-                          // Render the cell contents
-                          cell.render('Cell')
+                          // Render the header
+                          column.render('Header')
                         }
-                      </td>
-                    );
-                  })
-                }
-              </tr>
-            );
-          })
-        }
-      </tbody>
-    </table>
+                      </th>
+                    ))
+                  }
+                </tr>
+              ))
+            }
+          </thead>
+          {/* Apply the table body props */}
+          <tbody {...getTableBodyProps()}>
+            {
+              // Loop over the table rows
+              rows.map((row) => {
+                // Prepare the row for display
+                prepareRow(row);
+                return (
+                  // Apply the row props
+                  <tr {...row.getRowProps()}>
+                    {
+                      // Loop over the rows cells
+                      row.cells.map((cell) => {
+                        // Apply the cell props
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {
+                              // Render the cell contents
+                              cell.render('Cell')
+                            }
+                          </td>
+                        );
+                      })
+                    }
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
 
--- logs-reader/main.js
+++ logs-reader/main.js
@@ -11,63 +11,21 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => {
-  //   res.send("Hello World!");
-
+app.get("/logs", (req, res) => {
   readFromDatabase(function (response) {
     res.send(response);
   });
-
-  //   res.send();
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-// const couchdb = require("./couchdb"),
 const nano = require("nano")("http://admin:admin@127.0.0.1:5984"), // Not for production use :)
   per_page = 10,
   params = { include_docs: true, limit: per_page, descending: true };
-// var cdb = new couchdb.Db("logs");
-
-// const NodeCouchDb = require("node-couchdb");
-// const parser = require("./log-parser");
-
-// // node-couchdb instance with default options
-// const couch = new NodeCouchDb();
-
-// parser.process_logs("./logs/test_log.txt", parser.logParser, function (logs) {
-//   for (var i = 0, l = logs.length; i < l; i++) {
-//     var log = logs[i];
-//     // couch.insert("logs", log, function (err, res) {
-//     //   if (err) return console.log(err);
-//     //   console.log(res);
-//     // });
-//     cdb.put(uuid, log, function (err, result) {});
-//   }
-// });
 
 var config = require("./config").config;
-// couchdb = require("./lib/node-couchdb-min/couchdb"),
-// parser = require("./log-parser");
-
-// TODO: I NEED THIS
-// await checkDatabaseExists(async function () {
-//   const logsDatabase = nano.db.use(config.couch_db_name);
-//   await logsDatabase
-//     .insert({ foo: "bar" })
-//     .then((response) => {
-//       // p.process ... // TODO
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       throw err;
-//     });
-
-//   var debug = false;
-//   if (debug) parser.setDebug(debug);
-// });
 
 var init = async function () {
   await nano.db
@@ -87,37 +45,20 @@ var init = async function () {
 };
 
 var readFromDatabase = async function (callback) {
-  //   console.log("Processing " + sessions.length + " sessions");
-
-  //   for (var i = 0; i < sessions.length; i++) {
-  //     console.log(
-  //       "Processing " +
-  //         sessions[i].length +
-  //         " logs for session " +
-  //         (i + 1) +
-  //         " of " +
-  //         sessions.length
-  //     );
-
-  // TODO: Pass DB in a better way
   const logsDatabase = nano.db.use(config.couch_db_name);
 
   // TODO: Batch read
   //   for (const log of sessions[i]) {
   await logsDatabase
-    // .get("6b1de0ffe31b04474ec38a999e003b15") // ID
     .list(params)
     .then((response) => {
-      // p.process ... // TODO
-      console.log(response);
+      console.log(new Date() + " - Fetched " + response.rows.length + " logs");
       callback(response);
     })
     .catch((err) => {
       console.log(err);
       throw err;
     });
-  //   }
-  //   }
 };
 
 init();
